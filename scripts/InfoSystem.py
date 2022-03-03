a2e9f25f82737041f4e97e74d667e62fdb7ac5ff
btrantruong <27907677+btrantruong@users.noreply.github.com>
2022-03-03 14:53:05 -0500

    more verbose debugging

--- scripts/InfoSystem.py
+++ scripts/InfoSystem.py
@@ -36,6 +36,8 @@ class InfoSystem:
         
         #Keep track of number of memes globally
         self.num_memes=0
+        self.num_meme_unique=0
+        self.memes_human_feed = 0
         self.quality_diff = 1
         self.quality = 1
         self.time_step=0
@@ -55,6 +57,10 @@ class InfoSystem:
         # Try making 
         # bots = [n for n in G.nodes if G.nodes[n]['bot']==True]
         # humans = [n for n in G.nodes if G.nodes[n]['bot']==False]
+
+        #debug
+        # bao_indeg = []
+
         for agent in G.nodes:
             id = G.nodes[agent]['ID']
             friend_ids= [G.nodes[n]['ID'] for n in G.successors(agent)]
@@ -62,9 +68,17 @@ class InfoSystem:
 
             follower_ids = [G.nodes[n]['ID'] for n in G.predecessors(agent)]
             self.follower_info[id] = follower_ids
+            # if self.verbose:
+            #     bao_indeg+=[len(follower_ids)]
+
         self.n_agents = nx.number_of_nodes(G)
-        print('Finish initializing agents, total in graph: {}, in dict: {}'.format(self.n_agents, len(self.tracking_agents)))
-    
+        print('Initialized agents, total in original graph: {}, in Infosystem: {}'.format(self.n_agents, len(self.tracking_agents)))
+        
+        # if self.verbose:
+        #     in_deg = [deg for node, deg in G.in_degree(G.nodes())] #number of followers
+        #     print('Graph Avg in deg', round(sum(in_deg)/len(in_deg),2))
+        #     print('Info Sys in deg: ', round(sum(bao_indeg)/len(bao_indeg),2))
+
     def init_followers(self):
         for aidx, agent in self.tracking_agents.items():
             # if follower list hasn't been realized into Users(), do it
@@ -81,11 +95,11 @@ class InfoSystem:
         while self.quality_diff > self.epsilon: 
             if self.verbose:
                 # print('time_step = {}, q = {}, diff = {}'.format(self.time_step, self.quality, self.quality_diff), flush=True) 
-                print('time_step = {}, q = {}, diff = {}, memes = {}'.format(self.time_step, self.quality, self.quality_diff, self.num_memes), flush=True) 
+                print('time_step = {}, q = {}, diff = {}, unique/human memes = {}/{}, all memes created={}'.format(self.time_step, self.quality, self.quality_diff, self.num_meme_unique, self.memes_human_feed, self.num_memes), flush=True) 
             self.time_step += 1
             for _ in range(self.n_agents):
                 self.simulation_step()
-                self.update_quality()
+            self.update_quality()
 
             #TODO: track meme
             # b: Return net: no need because the network doesn't change. 
@@ -103,8 +117,8 @@ class InfoSystem:
             meme = random.choices(agent.feed, weights=[m.fitness for m in agent.feed], k=1)[0] #random choices return a list
         else:
             # new meme
-            self.num_memes+=1
-            meme = Meme(self.num_memes, is_by_bot=agent.is_bot, phi=self.phi)
+            self.num_meme_unique+=1
+            meme = Meme(self.num_meme_unique, is_by_bot=agent.is_bot, phi=self.phi)
         #TODO: bookkeeping
 
         # spread (truncate feeds at max len alpha)
@@ -115,10 +129,11 @@ class InfoSystem:
             
             if agent.is_bot==1:
                 follower.add_meme_to_feed(meme, n_copies = self.theta)
+                self.num_memes+=self.theta
             else:
                 follower.add_meme_to_feed(meme)
-
-            assert(len(follower.feed)==self.alpha)
+                self.num_memes+=1
+            assert(len(follower.feed)<=self.alpha)
 
     def update_quality(self):
         # use exponential moving average for convergence
@@ -132,12 +147,13 @@ class InfoSystem:
         # calculate meme quality for tracked Users
         total=0
         count=0
-        humans = [user for user in self.tracking_agents.values() if user.is_bot==0] #because quality of bot is 0
+        humans = [user for user in self.tracking_agents.values() if user.is_bot==0] 
         for user in humans:
             for meme in user.feed:
                 total += meme.quality
                 count +=1
-        return total / count
+        self.memes_human_feed = count
+        return total / count if count >0 else 0
     
     # calculate fraction of low-quality memes in system (for tracked User)
     #
