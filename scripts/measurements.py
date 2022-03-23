1242ecd9bad3f6b8fcb4926948bb4c7d8e96b93b
btrantruong <27907677+btrantruong@users.noreply.github.com>
2022-03-22 23:14:05 -0400

    analysis on files resulted from propagate.py

--- scripts/measurements.py
+++ scripts/measurements.py
@@ -6,31 +6,45 @@
 
 #TODO: Rewrite these methods for ig_InfoSys
 # calculate average quality of memes in system
+# count_bot=False
+def measure_average_quality(self):
+    # calculate meme quality for tracked Users
+    total=0
+    count=0
+    if self.mode=='igraph':
+        human_uids = [n['uid'] for n in self.network.vs if n['bot']==0]
+        for u in human_uids:
+            for meme in self.agent_feeds[u]:
+                total+= meme.quality
+                count+=1
+    else:
+        humans = [user for user in self.tracking_agents.values() if user.is_bot==0] 
+        for user in humans:
+            for meme in user.feed:
+                total += meme.quality
+                count +=1
+    self.memes_human_feed = count
+    return total / count if count >0 else 0
+
+# calculate fraction of low-quality memes in system (for tracked User)
 #
-def measure_average_quality(G, count_bot=False):
-    total = 0
+def measure_average_zero_fraction(self):
     count = 0
-    for agent in G.nodes:
-        if count_bot == True or G.nodes[agent]["bot"] == False:
-            for m in G.nodes[agent]["feed"]:
-                total += m[0]
-                count += 1
-    return total / count
+    zero_memes = 0 
 
+    if self.mode=='igraph':
+        human_uids = [n['uid'] for n in self.network.vs if n['bot']==0]
+        for u in human_uids:
+            zero_memes += sum([1 for meme in self.agent_feeds[u] if meme.quality==0])
+            count += len(self.agent_feeds[u])
+    else:
+        human_agents = [agent for agent in self.tracking_agents.values() if agent.is_bot==0]
+        for agent in human_agents:
+            zero_memes += sum([1 for meme in agent.feed if meme.quality==0])
+            count += len(agent.feed)
 
-# calculate fraction of low-quality memes in system
-#
-def measure_average_zero_fraction(G):
-    count = 0
-    zeros = 0
-    for agent in G.nodes:
-        if G.nodes[agent]["bot"] == False:
-            for m in G.nodes[agent]["feed"]:
-                count += 1
-                if m[0] == 0:
-                    zeros += 1
-    return zeros / count
-
+    return zero_memes / count
+    
 # relationship between indegree (#followers) and low-quality in humans
 #
 def quality_vs_degree(G):
