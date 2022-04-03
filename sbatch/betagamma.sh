b34186da7cb69052f99bd643b04b8af9379467fa
btrantruong <27907677+btrantruong@users.noreply.github.com>
2022-04-03 01:40:43 -0400

    sbatch script

---
+++ sbatch/betagamma.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#!/bin/bash
+#####  Constructed by HPC everywhere #####
+#SBATCH --mail-user=baotruon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=3-23:59:00
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=betagamma
+
+######  Module commands #####
+source /N/u/baotruon/Carbonate/miniconda3/etc/profile.d/conda.sh
+conda activate graph
+
+
+######  Job commands go below this line #####
+cd /N/u/baotruon/Carbonate/marketplace
+echo '###### vary beta gamma ######'
+snakemake --nolock --snakefile workflow/vary_betagamma.smk --cores 20
