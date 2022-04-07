1ef85f941ea45e6d17c00a7c31483d41da03d4fc
btrantruong <btrantruong@gmail.com>
2022-04-07 15:28:50 -0400

    fill_in_params() process data from multiple runs

--- workflow/makeplot.py
+++ workflow/makeplot.py
@@ -14,12 +14,13 @@ cmap = cm.get_cmap('inferno', 10)
 
 ABS_PATH = ''
 DATA_PATH = os.path.join(ABS_PATH, "data")
+RES_PATH = os.path.join(ABS_PATH, "results")
 
 all_configs = json.load(open(os.path.join(DATA_PATH,'all_configs.json'),'r'))
+config_fpath = os.path.join(DATA_PATH,'all_configs.json')
 
-RES_PATH = 'results/vary_phigamma_2runs'
 PLOT_PATH = 'results/plots'
-result_dir = RES_PATH
+
 
 pprint = {'gamma':'$\\gamma$','theta': '$\\theta$', 'beta':'$\\beta$', 'phi': '$\phi$', 
         'quality': 'Relative Average Quality', 'discriminative_pow': 'Discriminative Power', 'diversity': 'Diversity'}
@@ -29,9 +30,10 @@ GAMMA = [0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.02, 0.05, 0.1, 0.2, 0.5]
 TARGETING = [None, 'hubs', 'partisanship', 'conservative', 'liberal', 'misinformation']
 PHI_LIN = list(range(1,11))
 THETA = [1,2,4,6,8,10,12,14]
+
 heatmap_config = {'vary_thetagamma': {'x':'theta', 'y':'gamma', 'xvals':THETA, 'yvals':GAMMA},
-'vary_phigamma': {'x':'gamma', 'y':'phi', 'xvals':GAMMA, 'yvals':PHI_LIN},
-'vary_betagamma': {'x':'gamma', 'y':'beta', 'xvals':GAMMA, 'yvals':BETA}
+'vary_phigamma': {'x':'phi', 'y':'gamma', 'xvals':PHI_LIN, 'yvals':GAMMA},
+'vary_betagamma': {'x':'beta', 'y':'gamma', 'xvals':BETA, 'yvals':GAMMA}
 }
 
 def update_dict(adict, default_dict,fill_na=True):
@@ -48,50 +50,77 @@ def update_dict(adict, default_dict,fill_na=True):
 
 def heatmap_data(result_dir, exp_type='vary_thetagamma', cell_type='quality'):
     #2D array for plotting data 
-    respath = glob.glob('%s/*.json' %result_dir)
-    resfiles = [i.split('%s/' %result_dir)[1].replace('.json','') for i in respath]
-
-    all_results = [json.load(open(os.path.join(result_dir,'%s.json' %file),'r')) for file in resfiles]
+    print('Drawing %s plot' %exp_type)
+    _, all_results = fill_in_param(config_fpath, exp_type=exp_type)
 
     x = heatmap_config[exp_type]['x']
     y = heatmap_config[exp_type]['y']
 
     results = defaultdict(lambda: [])
-    for yval in heatmap_config[exp_type]['yvals']:
+    # for idx, yval in enumerate(heatmap_config[exp_type]['yvals']):
+    #     for xval in heatmap_config[exp_type]['xvals']:
+    #         if cell_type=='discriminative_pow':
+    #             results[yval] += [np.mean(res[cell_type][0]) for res in all_results if res[x]==xval and res[y]==yval]
+    #         else:
+    #             results[yval] += [np.mean(res[cell_type]) for res in all_results if res[x]==xval and res[y]==yval]
+    
+    for idx, yval in enumerate(heatmap_config[exp_type]['yvals']):
         for xval in heatmap_config[exp_type]['xvals']:
             if cell_type=='discriminative_pow':
-                results[yval] += [np.mean(res[cell_type][0]) for res in all_results if res[x]==xval and res[y]==yval]
+                vals = [res[cell_type][0] for res in all_results if res[x]==xval and res[y]==yval]
+                results[yval] += [np.mean(vals) if len(vals)>0 else 0]
             else:
-                results[yval] += [np.mean(res[cell_type]) for res in all_results if res[x]==xval and res[y]==yval]
-            
+                vals = [res[cell_type] for res in all_results if res[x]==xval and res[y]==yval]
+                results[yval] += [np.mean(vals) if len(vals)>0 else 0]
+
     data = np.array([row for row in results.values()])
     return data
 
+def update_results(adict, newres_dict):
+    measures = ['quality','diversity','discriminative_pow']
+    # Append a dict with results from another run
+    for m in measures:
+        adict[m].extend(newres_dict[m])
+    return adict
 
-def lineplot_data(result_dir, config_fpath=None, exp_type='vary_thetagamma', anchors=[('gamma', 0.01), ('gamma', 0.02)], 
-                        x='beta', y='quality', relative=False):
-    respath = glob.glob('%s/*.json' %result_dir)
-    resfiles = [i.split('%s/' %result_dir)[1].replace('.json','') for i in respath]
+def fill_in_param(config_fpath, exp_type='vary_betagamma'):
+    result_dir = RES_PATH
+    res_dirs = [os.path.join(result_dir, i) for i in os.listdir(result_dir) if os.path.isdir(os.path.join(result_dir,i)) and exp_type in i]
+    firstrun = res_dirs[0]
+    respath = glob.glob('%s/*.json' %firstrun)
+    resfiles = [i.split('%s/' %firstrun)[1].replace('.json','') for i in respath]
 
-    if config_fpath is not None:
-        #fill in other config
-        all_configs = json.load(open(config_fpath,'r'))
+    #fill in other config
+    all_configs = json.load(open(config_fpath,'r'))
 
-        params = {}
-        for file in resfiles: 
+    params = {}
+    results = {}
+
+    for file in resfiles: 
+        if file in all_configs[exp_type].keys():
             params[file] = all_configs[exp_type][file]
+            res = json.load(open(os.path.join(firstrun,'%s.json' %file),'r'))
+            res = update_dict(res, params[file])
+            nth_res = res
+            for dir in res_dirs[1:]:
+                paths = glob.glob('%s/*.json' %dir)
+                nth_resfiles = [i.split('%s/' %dir)[1].replace('.json','') for i in paths]
+                if file in nth_resfiles:
+                    nth_run = json.load(open(os.path.join(dir,'%s.json' %file),'r'))
+                    nth_res = update_results(res,nth_run)
+            results[file] = nth_res
+    
+    all_results = results.values()
+    return results, all_results
+
+def lineplot_data(config_fpath=None, exp_type='vary_thetagamma', anchors=[('gamma', 0.01), ('gamma', 0.02)], 
+                        x='beta', y='quality', relative=False):
+    print('Drawing %s plot' %exp_type)
+    results, _ = fill_in_param(config_fpath, exp_type=exp_type)
     
     multiline_data = []
     multiline_err = []
     for i,anchor in enumerate(anchors):
-        results = {}
-        for file in resfiles: 
-            res = json.load(open(os.path.join(result_dir,'%s.json' %file),'r'))
-            #TODO: Simplify this!
-            if config_fpath is not None:
-                results[file] = update_dict(res, params[file])
-            else: 
-                results[file] = res
         data = []
         for info in results.values():
             if info[anchor[0]]==anchor[1]:
@@ -101,7 +130,6 @@ def lineplot_data(result_dir, config_fpath=None, exp_type='vary_thetagamma', anc
                     data += [(info[x], info[y])] 
 #         data = [(info[x], info[y]) for info in results.values() if info[anchor[0]]==anchor[1]]
         
-
         avg_data = []
         y_err = []
         for xval,yval in data:
@@ -130,25 +158,23 @@ def plotmulti_infosys(ax, data, anchors=[('gamma', 0.01), ('gamma', 0.02)],
     
     ax.set_xlabel(pprint[x])
     ax.set_ylabel(pprint[y])
-    # ax.set_ylim(bottom=0)
     ax.legend()
-    # plt.savefig(os.path.join(result_dir, '%s%s.png' %(x,y)), dpi=300)
-    # # plt.show()
-    # plt.clf()
 
 def panel_plot(result_dir, config_fpath=None, exp_type= 'vary_thetagamma'):
     anchor_configs = {
         'vary_thetagamma': [('theta', val) for val in [1,6,14]],
-        'vary_phigamma': [('phi', val) for val in [1,5,10]]
+        'vary_phigamma': [('phi', val) for val in [1,5,10]],
+        'vary_betagamma': [('beta', val) for val in [0.001,0.05,0.1]]
     }
     xticks = {
         'vary_thetagamma': {'vals': THETA, 'label': pprint['theta']},
-        'vary_phigamma': {'vals': PHI_LIN, 'label': pprint['phi']}
+        'vary_phigamma': {'vals': PHI_LIN, 'label': pprint['phi']},
+        'vary_betagamma': {'vals': BETA, 'label': pprint['beta']},
     }
     measurements  = ['quality', 'diversity', 'discriminative_pow']
     anchors = anchor_configs[exp_type]
-    plots = [{'exp_type': 'vary_thetagamma', 'x': 'gamma', 'y': y} for y in measurements]
-    lineplots = [{'exp_type': 'vary_thetagamma', 'anchors':anchors, 'x': 'gamma', 'y': y} for y in measurements]
+    plots = [{'exp_type': exp_type, 'x': 'gamma', 'y': y} for y in measurements]
+    lineplots = [{'exp_type': exp_type, 'anchors':anchors, 'x': 'gamma', 'y': y} for y in measurements]
 
 
     params = {'axes.labelsize': 10,'axes.titlesize':10,'legend.fontsize': 10, 'xtick.labelsize': 10, 'ytick.labelsize': 10}
@@ -162,9 +188,14 @@ def panel_plot(result_dir, config_fpath=None, exp_type= 'vary_thetagamma'):
     laxs = [figure.add_subplot(3,2,i) for i in left]
 
     for plot_specs,ax in zip(lineplots, laxs):
-        line_data, _  = lineplot_data(result_dir, config_fpath, **plot_specs)
+        line_data, _  = lineplot_data(config_fpath, **plot_specs)
         plotmulti_infosys(ax, line_data, anchors=anchors, 
                             x=plot_specs['x'], y=plot_specs['y'], log_flag=True)
+
+    #TODO: Reduce tick size for betagamma
+    # if exp_type=='vary_betagamma':
+    #     ax.tick_params(axis='x', which='major', labelsize=5)
+    #     ax.tick_params(axis='x', which='minor', labelsize=5)
 
     raxs = [figure.add_subplot(3,2,i) for i in right]
     for map_spec, ax, cell in zip(plots, raxs, measurements): 
@@ -174,42 +205,6 @@ def panel_plot(result_dir, config_fpath=None, exp_type= 'vary_thetagamma'):
     figure.tight_layout() 
     plt.savefig(os.path.join(PLOT_PATH,'%s.png' %exp_type), dpi=100)
 
-#panel_plot(result_dir, config_fpath='data/all_configs.json', exp_type='vary_thetagamma')
-panel_plot(result_dir, config_fpath=None, exp_type='vary_phigamma')
-
-# anchors = [('theta', val) for val in [1,6,14]]
-# plots = [{'exp_type': 'vary_thetagamma', 'anchors': anchors, 'x': 'gamma', 'y':'quality'},
-#         {'exp_type': 'vary_thetagamma', 'anchors': anchors, 'x': 'gamma', 'y':'discriminative_pow'},
-#         {'exp_type': 'vary_thetagamma', 'anchors': anchors, 'x': 'gamma', 'y':'diversity'},
-#         ]
-
-# # plotmulti_infosys(RES_PATH, exp_type='vary_thetagamma', anchors=anchors, x='gamma', y='quality',log_flag=True)
-# params = {'axes.labelsize': 10,'axes.titlesize':10,'legend.fontsize': 10, 'xtick.labelsize': 10, 'ytick.labelsize': 10}
-
-# plt.rcParams.update(params)
-# figure = plt.figure(figsize=(10, 10), facecolor='w')
-
-# left=[1,3,5]
-# right=[2,4,6]
-
-# laxs = [figure.add_subplot(3,2,i) for i in left]
-
-# for plot_spec,ax in zip(plots, laxs):
-#     line_data, _  = lineplot_data(RES_PATH,config_fpath='data/all_configs.json', **plot_spec)
-#     plotmulti_infosys(ax, line_data, anchors=anchors, 
-#                         x='gamma', y='quality', log_flag=True)
-
-# raxs = [figure.add_subplot(3,2,i) for i in right]
-# maps = [{'exp_type': 'vary_thetagamma', 'x': 'gamma', 'y':'quality'},
-#         {'exp_type': 'vary_thetagamma', 'x': 'gamma', 'y':'discriminative_pow'},
-#         {'exp_type': 'vary_thetagamma', 'x': 'gamma', 'y':'diversity'},
-#         ]
-
-# cells = ['quality', 'discriminative_pow', 'diversity']
-# for map_spec,ax, cell in zip(maps, raxs, cells): 
-#     data = heatmap_data(result_dir, exp_type='vary_thetagamma', cell_type=cell)
-#     utils.draw_heatmap(ax, data, THETA, GAMMA, '$\\theta$', '$\gamma$', cmap, cell, vmax=None, vmin=None)
-
-# figure.tight_layout() 
-# plt.savefig(os.path.join(PLOT_PATH,'thetagamma.png'), dpi=100)
-# plt.show()
+# panel_plot(RES_PATH, config_fpath, exp_type='vary_phigamma')
+# panel_plot(RES_PATH, config_fpath, exp_type='vary_thetagamma')
+panel_plot(RES_PATH, config_fpath, exp_type='vary_betagamma')
