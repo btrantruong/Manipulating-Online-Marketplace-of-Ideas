b1a99d4afa776ebe8431518fb374cba867d39d0e
btrantruong <btrantruong@gmail.com>
2022-04-14 13:45:16 -0400

    exp sbatch

--- exps/test_targeting.sh
+++ exps/test_targeting.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-user=baotruon@iu.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=50gb
 #SBATCH --time=23:59:00
 #SBATCH --mail-type=FAIL,BEGIN,END
@@ -17,4 +17,4 @@ conda activate graph
 ######  Job commands go below this line #####
 cd /N/u/baotruon/Carbonate/marketplace
 echo '###### Save timestep quality of hub-targeting and no targeting ######'
-python3 exps/04132022_test_targeting.py
+python3 exps/04142022_trackpopularity.py
