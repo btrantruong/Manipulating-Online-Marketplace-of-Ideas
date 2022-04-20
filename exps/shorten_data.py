8596a440983d870acc2b9c37b3967759037299aa
btrantruong <btrantruong@gmail.com>
2022-04-20 15:34:10 -0400

    print

--- exps/shorten_data.py
+++ exps/shorten_data.py
@@ -12,15 +12,17 @@ RES_PATH = '/N/slate/baotruon/marketplace/results/vary_thetaphi_1runs'
 fpaths = glob.glob(os.path.join(RES_PATH,'long', '*.json.gz'))
 
 print('Separating %s files..' %len(fpaths))
+exists = 0
 for fpath in fpaths:
     fname = fpath.split('%s/long/' %RES_PATH)[1].replace('.json.gz','')
     newpath = os.path.join(RES_PATH,'%s.json' %fname)
     if utils.make_sure_file_exists(newpath):
+        exists+=1
         continue
     else:
         data = utils.read_json_compressed(fpath)
         data_short = {k:v for k,v in data.items() if k not in ['memes', 'feeds']}
         fout = gzip.open(newpath, 'w')
         utils.write_json_compressed(fout, data)
-
+print('%s files exists' %exists)
 print('Finish separating data!')
