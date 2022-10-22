3d7ae77e6bcf3fe60483e0548519b60cc43953ee
btrantruong <btrantruong@gmail.com>
2022-10-22 17:47:07 -0400

    baseline path name

--- sbatch/baseline.sh
+++ sbatch/baseline.sh
@@ -16,4 +16,4 @@ conda activate graph
 ######  Job commands go below this line #####
 cd /N/u/baotruon/Carbonate/marketplace
 echo '###### baseline ######'
-python3 workflow/scripts/driver.py -i /N/slate/baotruon/marketplace/data/igraph/vary_network/follower_network.gml -o /N/slate/baotruon/marketplace/newpipeline/results/10202022_baseline/baseline.json -v /N/slate/baotruon/marketplace/newpipeline/verbose/10202022_baseline/baseline.json.gz --config /N/slate/baotruon/marketplace/config_10222022_baseline/baseline/baseline.json --mode igraph --times 2
+python3 workflow/scripts/driver.py -i /N/slate/baotruon/marketplace/data/igraph/vary_network/follower_network.gml -o /N/slate/baotruon/marketplace/newpipeline/results/10202022_baseline/baseline.json -v /N/slate/baotruon/marketplace/newpipeline/verbose/10202022_baseline/baseline.json.gz --config /N/slate/baotruon/marketplace/config_10202022_baseline/baseline/baseline.json --mode igraph --times 2
