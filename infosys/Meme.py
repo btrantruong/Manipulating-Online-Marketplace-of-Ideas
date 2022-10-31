2330ce273a8ce117f809d22fc8f8437561ff973a
btrantruong <btrantruong@gmail.com>
2022-10-30 22:06:46 -0400

    keep phi constant for human memes

--- infosys/Meme.py
+++ infosys/Meme.py
@@ -1,31 +1,35 @@
-
 import random
+
 
 class Meme:
     def __init__(self, id, is_by_bot=0, phi=1):
         self.id = id
         self.is_by_bot = is_by_bot
-        self.phi = phi
-        quality, fitness  = self.get_values()
+        if self.is_by_bot == 0:
+            # Because phi is deception by bot, phi for human memes should stay constant
+            self.phi = 1
+        else:
+            self.phi = phi
+        quality, fitness = self.get_values()
         self.quality = quality
         self.fitness = fitness
-    
+
     # return (quality, fitness, id) meme tuple depending on bot flag
     # using https://en.wikipedia.org/wiki/Inverse_transform_sampling
-    # default phi = 1 is bot deception; >= 1: meme fitness higher than quality 
+    # default phi = 1 is bot deception; >= 1: meme fitness higher than quality
     # id: unique IDs
     def get_values(self):
-        if self.is_by_bot==1:
+        if self.is_by_bot == 1:
             exponent = 1 + (1 / self.phi)
         else:
             exponent = 1 + self.phi
-            
-        u = random.random()
-        fitness = 1 - (1 - u)**(1 / exponent)
 
-        if self.is_by_bot==1:
+        u = random.random()
+        fitness = 1 - (1 - u) ** (1 / exponent)
+
+        if self.is_by_bot == 1:
             quality = 0
         else:
             quality = fitness
-        
+
         return quality, fitness
