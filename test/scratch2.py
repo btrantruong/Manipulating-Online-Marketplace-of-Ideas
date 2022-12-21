aa24cd5863171e42f3d29cc57acbae4f6d944979
btrantruong <btrantruong@gmail.com>
2022-12-21 14:00:26 -0700

    cleanup

--- test/scratch2.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-from User import User 
-
-G = nx.read_gml('data/follower_network.gml.gz')
-# G = nx.read_gml('network.gml.gz')
-nodes = [n for n in G.nodes()]
-for agent in nodes:
-    name = G[agent]['ID']
